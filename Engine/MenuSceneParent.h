e08664041ac7fa00ba22ace531a724c133ca482d
zacharytay1994 <s10179991@connect.np.edu.sg>
2019-04-13 17:22:15 +0800

    made some slight changes to menu scene inheritance

--- Engine/MenuSceneParent.h
+++ Engine/MenuSceneParent.h
@@ -11,13 +11,26 @@
 
 class MenuSceneParent {
 public:
-	MenuSceneParent(const std::string& name, const std::string& filename)
+	MenuSceneParent(const std::string& name, const std::string& filename, Graphics& gfx)
 		:
 		name(name),
-		menuTexture(std::make_unique<Surface>(filename))
+		menuTexture(std::make_unique<Surface>(filename)),
+		gfx(gfx),
+		screenWidth(gfx.ScreenWidth),
+		screenHeight(gfx.ScreenHeight),
+		surfaceWidth(menuTexture->GetWidth()),
+		surfaceHeight(menuTexture->GetHeight()),
+		screenAlphaW(float(surfaceWidth) / float(screenWidth)),
+		screenAlphaH(float(surfaceHeight) / float(screenHeight))
 	{}
 	virtual void Update(Keyboard& kbd, Mouse& mouse, float dt) = 0;
-	virtual void Draw() = 0;
+	virtual void Draw() {
+		for (int x = 0; x < screenWidth; x++) {
+			for (int y = 0; y < screenHeight; y++) {
+				gfx.PutPixel(x, y, (Color)(menuTexture->GetPixel(int(x * screenAlphaW), int(y * screenAlphaH))));
+			}
+		}
+	};
 	virtual void OnClick() = 0;
 	virtual ~MenuSceneParent() = default;
 	const std::string& GetName() const {
@@ -28,4 +41,12 @@ public:
 private:
 	const std::string name;
 	const std::vector<Rect> clickableRects;
+	// draw screen variables
+	Graphics& gfx;
+	const int screenWidth;
+	const int screenHeight;
+	const int surfaceWidth;
+	const int surfaceHeight;
+	const float screenAlphaW;
+	const float screenAlphaH;
 };
